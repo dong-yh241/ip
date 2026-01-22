eda856d149b660050c9e7afd1491e914356633c5
浅蓝 <qianlan@MacBook-Pro-84.local>
2026-01-23 06:36:44 +0800

    Level-1: echo user input until bye

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,16 +1,31 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Cipher {
     private static final String LINE = "____________________________________________________________";
     private static final String NAME = "Cipher";
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println(LINE);
         System.out.println("Hello! I'm " + NAME);
         System.out.println("What can I do for you?");
         System.out.println(LINE);
 
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(LINE);
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(LINE);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            }
+
+            System.out.println(LINE);
+            System.out.println(input);
+            System.out.println(LINE);
+        }
+        sc.close();
     }
 }
