4a13e3d43f8691a1334bfca823d0d55b0f7f8973
浅蓝 <qianlan@MacBook-Pro-84.local>
2026-01-23 06:08:10 +0800

    Level-0: rename chatbot to Cipher

---
+++ src/main/java/Cipher.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Cipher {
+    private static final String LINE = "____________________________________________________________";
+    private static final String NAME = "Cipher";
+
+    public static void main(String[] args) {
+        System.out.println(LINE);
+        System.out.println("Hello! I'm " + NAME);
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+}
